ea1af9b2763011a6c43934560c600bb548025275
Aubree Abril <aubree.abril@gmail.com>
2017-12-13 03:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const app = "I don't do much."
 var array = []
 
 function destructivelyAppendKitten(name) {
-  array.push(name)
+  array.push(name, [0])
   return array
 }
 
-function
+function destructivelyPrependKitten(name) {
+  
+}
